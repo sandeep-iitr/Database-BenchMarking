d064955aa0917bd8338ab923aaf2456ce866faeb
Sandeep Singh Sandha <sandha.iitr@gmail.com>
2018-01-04 20:25:33 -0800

    date range added

--- src/main/java/com/database/test/Geomesa_simple_test.java
+++ src/main/java/com/database/test/Geomesa_simple_test.java
@@ -1,8 +1,3 @@
-/*
- * Author: Sandeep Singh Sandha
- * Created: 29 June, 2017
- */
-
 package com.database.test;
 
 import java.util.Random;
@@ -11,7 +6,6 @@ import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.omg.CORBA.INITIALIZE;
 
 import com.database.geomesa.GeomesaHbase;
 
@@ -19,6 +13,9 @@ public class Geomesa_simple_test {
 
 	static GeomesaHbase gmh;
 	static int count=300;//number of data points to insert
+	
+	static DateTime MIN_DATE = new DateTime(2014, 1, 1, 0, 0, 0, DateTimeZone.forID("UTC"));
+	static Long SECONDS_PER_WEEK =  7L * 24L * 60L * 60L;
 	
 	public static void main(String[] args) {	
 
@@ -41,9 +38,12 @@ public class Geomesa_simple_test {
 			if(result.size()<400)
 			System.out.println(": Result of _Box_Lat_Lng is:" + result.toJSONString());
 			
-			long date_min = 1499110975914L;//1499110975914 System.currentTimeMillis()-1000000;//past 100 seconds
-			long date_max = 1499110976514L;//1499110977514 System.currentTimeMillis();//1499110977514
 			
+			DateTime dateTime1 = MIN_DATE;
+			DateTime dateTime2 = dateTime1.plusSeconds((int) Math.round((SECONDS_PER_WEEK)));
+			
+			long date_min = dateTime1.getMillis();
+			long date_max = dateTime2.getMillis();
 			
 			result = gmh.Query_Date_Range(date_min,date_max);
 			System.out.println(": Side of Result _Date_Range :" + result.size());
@@ -54,9 +54,7 @@ public class Geomesa_simple_test {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
-		
-		
+	
 	}// end main
 	
 	
@@ -70,7 +68,6 @@ public class Geomesa_simple_test {
 	//inserting count data points in Geomesa
 	 static void insert_data(int count)
 	 {
-
 			double value_min=10.0;
 			double value_max=20.0;
 			Random random=new Random(5771);
@@ -78,16 +75,19 @@ public class Geomesa_simple_test {
 			double lat_min=30.0;
 			double lng_min=60.0;
 			double diff_loc=5.0;
-			
+					
 			for (int i = 0; i < count; i++)
 			{
 			JSONObject Data = new JSONObject();
 			String data_id = ""+i;
 			
 			double value = value_min+random.nextDouble()*(value_max-value_min);
-			long millis = System.currentTimeMillis();
 			
-			long unixTimestamp = millis;//note time is used in millisec in the System
+			DateTime dateTime = MIN_DATE.plusSeconds((int) Math.round(random.nextDouble() * SECONDS_PER_WEEK));
+			
+			//long millis = System.currentTimeMillis();
+			
+			long unixTimestamp = dateTime.getMillis();//note time is used in millisec in the System
 
 			double lat=lat_min+random.nextDouble()*diff_loc;
 			double lng=lng_min+random.nextDouble()*diff_loc;		
