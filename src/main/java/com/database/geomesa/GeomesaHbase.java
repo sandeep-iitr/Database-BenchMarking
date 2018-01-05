b86cb6615e4acd2c392af604202bf89e6ea6dfbf
Sandeep Singh Sandha <sandha.iitr@gmail.com>
2018-01-04 20:14:14 -0800

    GeoMesa

---
+++ src/main/java/com/database/geomesa/GeomesaHbase.java
@@ -0,0 +1,301 @@
+package com.database.geomesa;
+
+import com.beust.jcommander.internal.Lists;
+import com.google.common.base.Joiner;
+import com.vividsolutions.jts.geom.Geometry;
+import org.geotools.data.*;
+import org.geotools.factory.Hints;
+import org.geotools.feature.DefaultFeatureCollection;
+import org.geotools.feature.FeatureCollection;
+import org.geotools.feature.FeatureIterator;
+import org.geotools.feature.SchemaException;
+import org.geotools.feature.simple.SimpleFeatureBuilder;
+import org.geotools.filter.text.cql2.CQL;
+import org.geotools.filter.text.cql2.CQLException;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.locationtech.geomesa.utils.text.WKTUtils$;
+import org.opengis.feature.Feature;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.filter.Filter;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class GeomesaHbase {
+
+	//DataStore is from Geotools, here it is used to return an indexed datastore.
+	DataStore dataStore=null;
+	static String simpleFeatureTypeName = "GeoMesaTable";
+	static SimpleFeatureBuilder featureBuilder=null;
+	
+	public void geomesa_initialize() {
+			
+			try {
+				if (dataStore == null) {
+					Map<String, Serializable> parameters = new HashMap<>();
+					parameters.put("bigtable.table.name", "Geomesa");
+					
+					//DataStoreFinder is from Geotools, returns an indexed datastore if one is available.
+					dataStore = DataStoreFinder.getDataStore(parameters);
+				}
+	
+				assert dataStore != null;
+	
+				// establish specifics concerning the SimpleFeatureType to store
+				String simpleFeatureTypeName = "GeoMesaTable";
+				SimpleFeatureType simpleFeatureType = createSimpleFeatureType();
+	
+				// write Feature-specific metadata to the destination table in HBase
+				// (first creating the table if it does not already exist); you only
+				// need
+				// to create the FeatureType schema the *first* time you write any
+				// Features
+				// of this type to the table
+				System.out.println("Creating feature-type (schema):  " + simpleFeatureTypeName);
+				dataStore.createSchema(simpleFeatureType);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+	
+		}
+
+	static SimpleFeatureType createSimpleFeatureType() throws SchemaException {
+		
+		/*
+		 * We use the DataUtilities class from Geotools to create a FeatureType that will describe the data
+		 * 
+		 */
+		
+		SimpleFeatureType simpleFeatureType = DataUtilities.createType(simpleFeatureTypeName,
+				"point_loc:Point:srid=4326,"+// a Geometry attribute: Point type
+				"data_id:String,"+// a String attribute
+				"value:java.lang.Double,"+// a Long number attribute
+				"date:Date"// a date attribute for time
+				);
+		
+		return simpleFeatureType;
+		
+		
+	}
+
+	static FeatureCollection createNewFeatures(SimpleFeatureType simpleFeatureType, String data) {
+	
+		DefaultFeatureCollection featureCollection = new DefaultFeatureCollection();
+		
+		if(featureBuilder==null)
+		featureBuilder = new SimpleFeatureBuilder(simpleFeatureType);
+		
+		SimpleFeature simpleFeature=featureBuilder.buildFeature(null);
+		
+		/*
+		 * "name:String", "value:java.lang.Double", "date:Date",
+		 * "point_loc:Point:srid=4326", "data_type:String", "unit:String"
+		 */
+		
+		try {
+			JSONParser parser = new JSONParser();
+			JSONObject obj2 = (JSONObject) parser.parse(data);
+			String name = (String) obj2.get("data_id");
+			JSONObject loc = (JSONObject) obj2.get("location");
+			double lat = (Double) loc.get("lat");
+			double lng = (Double) loc.get("lng");
+			Long timestamp = (Long) obj2.get("timeStamp");
+			
+			double value = (Double) obj2.get("value");
+			DateTime dateTime = new DateTime(timestamp);
+			
+			Geometry geometry = WKTUtils$.MODULE$.read("POINT(" + lat + " " + lng + ")");
+			
+			simpleFeature.setAttribute("data_id", name);
+			simpleFeature.setAttribute("value", value);
+			simpleFeature.setAttribute("point_loc", geometry);
+			simpleFeature.setAttribute("date", dateTime.toDate());
+
+			// accumulate this new feature in the collection
+			featureCollection.add(simpleFeature);
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+		return featureCollection;
+	}
+
+	static void insertFeatures(DataStore dataStore, FeatureCollection featureCollection)
+			throws IOException {
+
+		FeatureStore featureStore = (FeatureStore) dataStore.getFeatureSource(simpleFeatureTypeName);
+		featureStore.addFeatures(featureCollection);
+	}
+
+
+	
+	public void geomesa_insertData(String data) {
+		// find out where -- in HBase -- the user wants to store data
+
+		try {
+
+			if (dataStore == null) {
+				geomesa_initialize();
+			}
+
+			// establish specifics concerning the SimpleFeatureType to store
+			String simpleFeatureTypeName = "GeoMesaTable";
+			SimpleFeatureType simpleFeatureType = createSimpleFeatureType();
+
+			// create new features locally, and add them to this table
+			System.out.println("Creating new features");
+			FeatureCollection featureCollection = createNewFeatures(simpleFeatureType, data);
+			System.out.println("Inserting new features");
+			insertFeatures(dataStore, featureCollection);
+			System.out.println("done inserting Data");
+
+			/*
+			 * //querying Data now, results as shown below:
+			 * System.out.println("querying Data now, results as shown below:");
+			 * Query();
+			 */
+			System.out.println("Done");
+
+		} // end try
+		catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+	}// end function
+
+	static JSONArray queryFeatures_Box_Lat_Lng(DataStore dataStore, String geomField, double x0,
+			double y0, double x1, double y1) throws CQLException, IOException {
+
+		// construct a (E)CQL filter from the search parameters,
+		// and use that as the basis for the query
+		String cqlGeometry = "BBOX(" + geomField + ", " + x0 + ", " + y0 + ", " + x1 + ", " + y1 + ")";
+		Filter cqlFilter = CQL.toFilter(cqlGeometry);
+		
+		Query query = new Query(simpleFeatureTypeName, cqlFilter);
+
+		// submit the query, and get back an iterator over matching features
+		FeatureSource featureSource = dataStore.getFeatureSource(simpleFeatureTypeName);
+		FeatureIterator featureItr = featureSource.getFeatures(query).features();
+
+
+		JSONArray ja = new JSONArray();
+
+		// loop through all results
+		int n = 0;
+		while (featureItr.hasNext()) {
+			Feature feature = featureItr.next();
+
+			JSONObject Data = new JSONObject();
+			Data.put("data_id", feature.getProperty("data_id").getValue());
+			Data.put("value", feature.getProperty("value").getValue());
+			Data.put("date", feature.getProperty("date").getValue());
+			Data.put("point_loc", feature.getProperty("point_loc").getValue());
+			ja.add(Data);
+			
+			
+		}
+		featureItr.close();
+
+		return ja;
+	}
+
+	
+
+	
+	public JSONArray Query_Box_Lat_Lng(double lat_min, double lat_max, double lng_min, double lng_max) {
+		try {
+
+			if (dataStore == null) {
+				geomesa_initialize();
+			}
+		
+
+			// query a few Features from this table
+			System.out.println("Submitting query");
+			JSONArray result = queryFeatures_Box_Lat_Lng(dataStore, "point_loc", lat_min, lng_min, lat_max, lng_max);
+
+			return result;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+
+	}
+	
+
+	static JSONArray queryFeatures_Date_Range(DataStore dataStore, String dateField,long date_min2,long date_max2) throws CQLException, IOException {
+
+		// construct a (E)CQL filter from the search parameters,
+		// and use that as the basis for the query
+		DateTime date_min = new DateTime(date_min2);
+		DateTime date_max = new DateTime(date_max2);
+		
+		String cqlDates = "( " + dateField + " DURING " + date_min+" / " + date_max+")";
+		
+		
+		System.out.println("cqlDate: "+cqlDates);
+		
+		Filter cqlFilter = CQL.toFilter(cqlDates);
+		
+		
+		Query query = new Query(simpleFeatureTypeName, cqlFilter);
+
+		// submit the query, and get back an iterator over matching features
+		FeatureSource featureSource = dataStore.getFeatureSource(simpleFeatureTypeName);
+		FeatureIterator featureItr = featureSource.getFeatures(query).features();
+
+
+		JSONArray ja = new JSONArray();
+
+		// loop through all results
+		int n = 0;
+		while (featureItr.hasNext()) {
+			Feature feature = featureItr.next();
+
+			JSONObject Data = new JSONObject();
+			Data.put("data_id", feature.getProperty("data_id").getValue());
+			Data.put("value", feature.getProperty("value").getValue());
+			Data.put("date", feature.getProperty("date").getValue());
+			Data.put("point_loc", feature.getProperty("point_loc").getValue());
+			ja.add(Data);
+			
+			
+		}
+		featureItr.close();
+
+		return ja;
+	}
+	
+	public JSONArray Query_Date_Range(long date_min,long date_max) {
+		try {
+
+			if (dataStore == null) {
+				geomesa_initialize();
+			}
+		
+
+			// query a few Features from this table
+			System.out.println("Submitting Date query");
+			JSONArray result = queryFeatures_Date_Range(dataStore, "date",date_min,date_max);
+
+			return result;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+
+	}
+	
+	
+}// end class
---
+++ src/main/java/com/database/test/Geomesa_simple_test.java
@@ -0,0 +1,113 @@
+/*
+ * Author: Sandeep Singh Sandha
+ * Created: 29 June, 2017
+ */
+
+package com.database.test;
+
+import java.util.Random;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.omg.CORBA.INITIALIZE;
+
+import com.database.geomesa.GeomesaHbase;
+
+public class Geomesa_simple_test {
+
+	static GeomesaHbase gmh;
+	static int count=300;//number of data points to insert
+	
+	public static void main(String[] args) {	
+
+		try {
+			
+			// initializing the schema
+			 initialize();
+			
+			// inserting data points
+			 insert_data(count);
+			
+			// querying the data points
+			double lat_min=33.0;
+			double lat_max=33.1;
+			double lng_min=62.0;
+			double lng_max=62.1;
+			
+			JSONArray result = gmh.Query_Box_Lat_Lng(lat_min, lat_max, lng_min, lng_max);
+			System.out.println(": Side of Result _Box_Lat_Lng :" + result.size());
+			if(result.size()<400)
+			System.out.println(": Result of _Box_Lat_Lng is:" + result.toJSONString());
+			
+			long date_min = 1499110975914L;//1499110975914 System.currentTimeMillis()-1000000;//past 100 seconds
+			long date_max = 1499110976514L;//1499110977514 System.currentTimeMillis();//1499110977514
+			
+			
+			result = gmh.Query_Date_Range(date_min,date_max);
+			System.out.println(": Side of Result _Date_Range :" + result.size());
+			if(result.size()<400)
+			System.out.println(": Result of _Date_Range is:" + result.toJSONString());
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		
+		
+	}// end main
+	
+	
+	 static void initialize()
+	 {
+	   gmh = new GeomesaHbase();
+	   gmh.geomesa_initialize();
+	 }
+	
+	
+	//inserting count data points in Geomesa
+	 static void insert_data(int count)
+	 {
+
+			double value_min=10.0;
+			double value_max=20.0;
+			Random random=new Random(5771);
+			
+			double lat_min=30.0;
+			double lng_min=60.0;
+			double diff_loc=5.0;
+			
+			for (int i = 0; i < count; i++)
+			{
+			JSONObject Data = new JSONObject();
+			String data_id = ""+i;
+			
+			double value = value_min+random.nextDouble()*(value_max-value_min);
+			long millis = System.currentTimeMillis();
+			
+			long unixTimestamp = millis;//note time is used in millisec in the System
+
+			double lat=lat_min+random.nextDouble()*diff_loc;
+			double lng=lng_min+random.nextDouble()*diff_loc;		
+			
+			Data.put("data_id", data_id);
+			Data.put("value", value);
+			Data.put("timeStamp", unixTimestamp);
+
+			JSONObject location = new JSONObject();
+			location.put("lat", lat);
+			location.put("lng", lng);
+
+			Data.put("location", location);
+
+			System.out.println(i+ ": Data Point to Insert is:" + Data.toString());
+            gmh.geomesa_insertData(Data.toString());
+         
+			}//end for
+			
+	 }
+	
+
+}// end class
